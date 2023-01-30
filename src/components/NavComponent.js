cbf2a30b4edaf12cb4d9d356d38c1ec0062693dc
Adrian-Walker <awalker3365@gmail.com>
2023-01-29 21:47:58 -0500

    added modal to NavComponent

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,19 +1,29 @@
 import { Button, Navbar, Container, Modal, NavbarBrand } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+import { useState } from "react";
 
 function NavComponent() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Navbar expand="sm">
         <Navbar.Brand href="/">My Store</Navbar.Brand>
         <NavbarToggle />
         <NavbarCollapse className="justify-content-end">
-          <Button>Cart 0 Items</Button>
+          <Button onClick={handleShow}>Cart 0 Items</Button>
         </NavbarCollapse>
       </Navbar>
-      <Modal show={show} onHide={handleHide}>
-        
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>Modal Body</h1>
+        </Modal.Body>
       </Modal>
     </>
   );
